312905751228d4854ef9db55cf17d3a8a56ba490
edgar256 <tinka.edgar@gmail.com>
2022-12-23 14:56:07 +0300

    PATCHED VISA CARD ERROR

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 // COMPONENT IMPORTS
 import AppDrawerStack from './navigations/AppDrawerStack';
@@ -37,136 +38,138 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'SplashScreen'}>
-        <Stack.Screen
-          name="SplashScreen"
-          options={{
-            headerShown: false,
-          }}
-          component={SplashScreen}
-        />
-        <Stack.Screen
-          name="Start"
-          options={{
-            headerShown: false,
-          }}
-          component={Start}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{
-            headerShown: false,
-          }}
-          component={Login}
-        />
-        <Stack.Screen
-          name="Register"
-          options={{
-            headerShown: false,
-          }}
-          component={Register}
-        />
-        <Stack.Screen
-          name="HomeScreen"
-          options={{
-            headerShown: false,
-          }}
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="InboxDetails"
-          options={{
-            headerShown: false,
-          }}
-          component={InboxDetails}
-        />
-        <Stack.Screen
-          name="BusinessProfile"
-          options={{
-            headerShown: false,
-          }}
-          component={BusinessProfile}
-        />
-        <Stack.Screen
-          name="BusinessProfileReviews"
-          options={{
-            headerShown: false,
-          }}
-          component={BusinessProfileReviews}
-        />
-        <Stack.Screen
-          name="BusinessProfileAnalytics"
-          options={{
-            headerShown: false,
-          }}
-          component={BusinessProfileAnalytics}
-        />
-        <Stack.Screen
-          name="EventProfile"
-          options={{
-            headerShown: false,
-          }}
-          component={EventProfile}
-        />
-        <Stack.Screen
-          name="EventProfileAnalytics"
-          options={{
-            headerShown: false,
-          }}
-          component={EventProfileAnalytics}
-        />
-        <Stack.Screen
-          name="EventProfileReviews"
-          options={{
-            headerShown: false,
-          }}
-          component={EventProfileReviews}
-        />
-        <Stack.Screen
-          name="JobProfile"
-          options={{
-            headerShown: false,
-          }}
-          component={JobProfile}
-        />
-        <Stack.Screen
-          name="JobProfileAnalytics"
-          options={{
-            headerShown: false,
-          }}
-          component={JobProfileAnalytics}
-        />
-        <Stack.Screen
-          name="NewsProfile"
-          options={{
-            headerShown: false,
-          }}
-          component={NewsProfile}
-        />
-        <Stack.Screen
-          name="NewsProfileAnalytics"
-          options={{
-            headerShown: false,
-          }}
-          component={NewsProfileAnalytics}
-        />
-        <Stack.Screen
-          name="NewsProfileComments"
-          options={{
-            headerShown: false,
-          }}
-          component={NewsProfileComments}
-        />
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={'SplashScreen'}>
+          <Stack.Screen
+            name="SplashScreen"
+            options={{
+              headerShown: false,
+            }}
+            component={SplashScreen}
+          />
+          <Stack.Screen
+            name="Start"
+            options={{
+              headerShown: false,
+            }}
+            component={Start}
+          />
+          <Stack.Screen
+            name="Login"
+            options={{
+              headerShown: false,
+            }}
+            component={Login}
+          />
+          <Stack.Screen
+            name="Register"
+            options={{
+              headerShown: false,
+            }}
+            component={Register}
+          />
+          <Stack.Screen
+            name="HomeScreen"
+            options={{
+              headerShown: false,
+            }}
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="InboxDetails"
+            options={{
+              headerShown: false,
+            }}
+            component={InboxDetails}
+          />
+          <Stack.Screen
+            name="BusinessProfile"
+            options={{
+              headerShown: false,
+            }}
+            component={BusinessProfile}
+          />
+          <Stack.Screen
+            name="BusinessProfileReviews"
+            options={{
+              headerShown: false,
+            }}
+            component={BusinessProfileReviews}
+          />
+          <Stack.Screen
+            name="BusinessProfileAnalytics"
+            options={{
+              headerShown: false,
+            }}
+            component={BusinessProfileAnalytics}
+          />
+          <Stack.Screen
+            name="EventProfile"
+            options={{
+              headerShown: false,
+            }}
+            component={EventProfile}
+          />
+          <Stack.Screen
+            name="EventProfileAnalytics"
+            options={{
+              headerShown: false,
+            }}
+            component={EventProfileAnalytics}
+          />
+          <Stack.Screen
+            name="EventProfileReviews"
+            options={{
+              headerShown: false,
+            }}
+            component={EventProfileReviews}
+          />
+          <Stack.Screen
+            name="JobProfile"
+            options={{
+              headerShown: false,
+            }}
+            component={JobProfile}
+          />
+          <Stack.Screen
+            name="JobProfileAnalytics"
+            options={{
+              headerShown: false,
+            }}
+            component={JobProfileAnalytics}
+          />
+          <Stack.Screen
+            name="NewsProfile"
+            options={{
+              headerShown: false,
+            }}
+            component={NewsProfile}
+          />
+          <Stack.Screen
+            name="NewsProfileAnalytics"
+            options={{
+              headerShown: false,
+            }}
+            component={NewsProfileAnalytics}
+          />
+          <Stack.Screen
+            name="NewsProfileComments"
+            options={{
+              headerShown: false,
+            }}
+            component={NewsProfileComments}
+          />
 
-        <Stack.Screen
-          name="AppDrawerStack"
-          options={{
-            headerShown: false,
-          }}
-          component={AppDrawerStack}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="AppDrawerStack"
+            options={{
+              headerShown: false,
+            }}
+            component={AppDrawerStack}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
--- screens/NewItem.js
+++ screens/NewItem.js
@@ -24,18 +24,7 @@ import {launchImageLibrary} from 'react-native-image-picker';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import moment from 'moment';
 import Config from 'react-native-config';
-// import { CardField, useStripe } from '@stripe/stripe-react-native';
-
-// import {
-//   CreditCardInput,
-//   LiteCreditCardInput,
-// } from 'react-native-credit-card-input-plus';
-
-// import {CreditCardInput} from 'react-native-credit-card-input';
-// import {
-//   CreditCardInput,
-//   LiteCreditCardInput,
-// } from 'react-native-credit-card-input-plus';
+import {CreditCardInput} from 'react-native-credit-card-input';
 
 // ENVIRONMENT VARIABLES
 import {stripeSecretKey, stripePublishableKey} from 'react-native-dotenv';
@@ -51,11 +40,9 @@ import {
   CustomTextInput,
 } from '../components';
 
+const windowWidth = Dimensions.get('window').width;
+
 function NewItem() {
-  console.log({
-    stripeSecretKey,
-    stripePublishableKey,
-  });
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>News Item Screen!</Text>
@@ -85,5 +72,119 @@ function NewItem() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.grayDark,
+    height: '100%',
+    width: '100%',
+  },
+  inputContainer: {
+    padding: 10,
+    marginVertical: 0,
+  },
+  textWhite: {
+    color: COLORS.white,
+  },
+  labelText: {
+    color: COLORS.white,
+    fontSize: SIZES.normal,
+  },
+  textYellow: {
+    color: COLORS.yellow,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  imageText: {
+    flexDirection: 'row',
+    paddingVertical: 5,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    padding: 20,
+  },
+  btnContainerInner: {
+    paddingHorizontal: 5,
+    width: '50%',
+  },
+  button: {
+    backgroundColor: COLORS.black,
+    padding: 10,
+    width: 200,
+    marginBottom: 50,
+    marginHorizontal: '25%',
+  },
+  groupInput: {
+    flexDirection: 'row',
+    paddingVertical: 5,
+    marginLeft: 20,
+  },
+  phoneInput: {
+    flexDirection: 'row',
+    paddingVertical: 5,
+  },
+  centeredView: {
+    flex: 1,
+    marginTop: 0,
+    backgroundColor: '#fff',
+  },
+  modalView: {
+    margin: 0,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 15,
+    width: windowWidth,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: COLORS.yellow,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 2,
+  },
+  submitButton: {
+    backgroundColor: COLORS.black,
+    padding: 10,
+    width: 250,
+    marginTop: 20,
+    marginBottom: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  inputContainerStyle: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    flexDirection: 'column',
+    width: '100%',
+  },
+  inputStyle: {
+    backgroundColor: '#222242',
+    paddingLeft: 15,
+    borderRadius: 5,
+    color: '#fff',
+    // width:50
+  },
+  labelStyle: {
+    marginBottom: 5,
+    fontSize: 12,
+  },
+});
 
 export default NewItem;
